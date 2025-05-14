cf77623748195d485a9758397328d0604ba5d785
Mike Kozlyukov <polkandra@gmail.com>
2025-05-14 14:38:14 +0300

    Initial commit

---
+++ SimpleWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleWeather
+//
+//  Created by Michael Kozlyukov on 14.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
